2c06f127a690fbee7f5e8ce9b137781d3cb62b4f
zxy <foreverlemon99@gmail.com>
2024-03-04 16:24:17 +0800

    "master"

--- build/imu_yesense_ros/CMakeFiles/imu_yesense_ros_generate_messages_cpp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/zxy/IMU_test/devel/include/imu_yesense_ros/ImuExtended.h"
-  "CMakeFiles/imu_yesense_ros_generate_messages_cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/imu_yesense_ros_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
