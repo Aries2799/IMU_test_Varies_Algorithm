4726dcf8b12f1d15d69f0557069d2ad5975a0fe8
zxy <foreverlemon99@gmail.com>
2024-03-05 08:55:13 +0800

    修改：     src/imu_yesense_ros/src/main.cpp

--- src/imu_yesense_ros/src/main.cpp
+++ src/imu_yesense_ros/src/main.cpp
@@ -1,25 +1,24 @@
 #include "ros/ros.h"
-#include "imu_yesense_ros/attitude.h"  // 替换<your_package_name>为你的包名
+#include "imu_yesense_ros/attitude.h" 
 #include "AttitudeCalculator.h"
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "attitude_publisher");
     ros::NodeHandle nh;
 
-    AttitudeCalculator calculator; // 假设这已经启动了IMU数据读取和姿态计算
+    AttitudeCalculator calculator; 
     ros::Rate loop_rate(100); 
     ros::Publisher attitude_pub = nh.advertise<imu_yesense_ros::attitude>("attitude", 1000);
 
     while (ros::ok()) {
         Attitude latestAttitude = calculator.getLatestAttitude();
 
-        // 填充自定义消息
+
         imu_yesense_ros::attitude msg;
         msg.roll = latestAttitude.roll;
         msg.pitch = latestAttitude.pitch;
         msg.yaw = latestAttitude.yaw;
 
-        // 发布姿态信息
         attitude_pub.publish(msg);
 
         // ROS_INFO("Published Attitude - Roll: %f, Pitch: %f, Yaw: %f", msg.roll, msg.pitch, msg.yaw);
